6693613ebf9b4bd7dd3248ed2cabe95ec622b0a6
Tom-Procter <proctertj@hotmail.com>
2016-02-13 18:44:07 -0800

    Update to bootstrap example
    Making changes so that it actually looks nice

--- js/citations.js
+++ js/citations.js
@@ -1,63 +1,169 @@
-var width = $(window).width();
-var height = $(window).height()-50;
+var windowwidth = $(window).width();
+var windowheight = $(window).height();
+var width = 800;
+var height = 400;
+if (height > windowheight){
+  height = windowheight*2/3;
+}
+if (width > windowwidth){
+  width = windowwidth*2/3;
+}
 
-var sortByAuthor = false;
-var sortByYear = false;
+var sortByAuthorX = false;
+var sortByYearX = false;
+var sortByCitationsX = false;
+var sortByAuthorY = false;
+var sortByYearY = false;
+var sortByCitationsY = false;
+var authorLabels = ["First Author:", "Contributing Author:"];
+var yearLabels = ["2011:","2012:","2013:","2014:","2015:","2016:"];
 
-function showAll(){
-    sortByAuthor = false;
-    sortByYear = false;
+var maxCitations = 0;
+var minCitations = 0;
+var maxYear = 0;
+var minYear = 3000;
+
+function showAllX(){
+    sortByAuthorX = false;
+    sortByYearX = false;
+    sortByCitationsX = false;
+    d3.select("#labels").remove();
   }
 
-function viewByAuthor(){
-    sortByAuthor = true;
-    sortByYear = false;
+function viewByAuthorX(){
+    sortByAuthorX = true;
+    sortByYearX = false;
+    sortByCitationsX = false;
+    d3.select("#labels").remove();
   }
 
-function viewByYear(){
-    sortByYear = true;
-    sortByAuthor = false;
+function viewByYearX(){
+    sortByYearX = true;
+    sortByAuthorX = false;
+    sortByCitationsX = false;
+    d3.select("#labels").remove();
   }
+
+function viewByCitationsX(){
+  sortByYearX = false;
+  sortByAuthorX = false;
+  sortByCitationsX = true;
+  d3.select("#labels").remove();
+}
+
+function showAllY(){
+    sortByAuthorY = false;
+    sortByYearY = false;
+    sortByCitationsY = false;
+    d3.select("#labels").remove();
+  }
+
+function viewByAuthorY(){
+    sortByAuthorY = true;
+    sortByYearY = false;
+    sortByCitationsY = false;
+    d3.select("#labels").remove();
+  }
+
+function viewByYearY(){
+    sortByYearY = true;
+    sortByAuthorY = false;
+    sortByCitationsY = false;
+    d3.select("#labels").remove();
+  }
+
+function viewByCitationsY(){
+  sortByYearY = false;
+  sortByAuthorY = false;
+  sortByCitationsY = true;
+  d3.select("#labels").remove();
+}
+
+function addButton(div,value,buttonfunction){
+  div.append("input")
+    .attr("type","button")
+    .attr("class","button")
+    .attr("value",value)
+    .attr("onclick",buttonfunction);
+}
 
 console.log(width);
 console.log(height);
 
 var rscale = d3.scale.linear();
 
-rscale.domain([0,5]).range([0,100]);
+rscale.domain([0,5]).range([0,80]);
 
 function showCitations(data){
 
-  var buttons = d3.select("body").append("g");
+  for (var i=0; i<data.length; i++){
+    if (data[i].CitedBy > maxCitations){
+      maxCitations = data[i].CitedBy;
+    }
+  }
 
-  buttons.append("input")
-    .attr("type","button")
-    .attr("class","button")
-    .attr("value","View All")
-    .attr("onclick","showAll()");
+  for (var i=0; i<data.length; i++){
+    if (data[i].Year > maxYear){
+      maxYear = data[i].Year;
+    }
+    if (data[i].Year < minYear){
+      minYear = data[i].Year;
+    }
+  }
 
-  buttons.append("input")
-    .attr("type","button")
-    .attr("class","button")
-    .attr("value","View My Work")
-    .attr("onclick","viewByAuthor()");
+  var xbuttons = d3.select("#xbuttons").append("g").attr("class","buttons");
+  var ybuttons = d3.select("#ybuttons").append("g").attr("class","buttons");
 
-    buttons.append("input")
-    .attr("type","button")
-    .attr("class","button")
-    .attr("value","View By Year")
-    .attr("onclick","viewByYear()");
+  addButton(xbuttons,"Don't Sort","showAllX()");
+  addButton(xbuttons,"Sort by Author","viewByAuthorX()");
+  addButton(xbuttons,"Sort by Year","viewByYearX()");
+  addButton(xbuttons,"Sort by Citations","viewByCitationsX()");
 
   var force = d3.layout.force()
     .nodes(data)
     .size([width, height])
-    .charge(-200)
+    .charge(-150)
     .on("tick", tick)
     .start();
 
-  var svg = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+  var svg = d3.select("#citations").append("svg")
+    .attr("id","svg")
+    .attr("class","svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  d3.select("#svg").append("line")
+    .attr("x1", 0)
+    .attr("x2", width)
+    .attr("y1", height)
+    .attr("y2", height)
+    .attr("stroke-width",1)
+    .attr("stroke","black");
+
+  d3.select("#svg").append("line")
+    .attr("x1", 0)
+    .attr("x2", width)
+    .attr("y1", 0)
+    .attr("y2", 0)
+    .attr("stroke-width",1)
+    .attr("stroke","black");
+
+  d3.select("#svg").append("line")
+    .attr("x1", 0)
+    .attr("x2", 0)
+    .attr("y1", 0)
+    .attr("y2", height)
+    .attr("stroke-width",1)
+    .attr("stroke","black");
+
+  d3.select("#svg").append("line")
+    .attr("x1", width)
+    .attr("x2", width)
+    .attr("y1", 0)
+    .attr("y2", height)
+    .attr("stroke-width",1)
+    .attr("stroke","black");
+
 
   var text = svg.append("g").selectAll("text")
     .data(force.nodes())
@@ -74,6 +180,7 @@ function showCitations(data){
     .append("a")
     .attr("xlink:href", function(d){ return d.URL;})
     .append("circle")
+    .attr("class","circles")
     .attr("r", function(d,i) { return rscale(Math.sqrt(d.CitedBy/Math.PI)); })
     .style("fill", function(d, i) {
       if (d.Label == "Thesis"){
@@ -84,7 +191,6 @@ function showCitations(data){
       }
       else { return 'rgba(255, 0, 0, 0.3)'}
     })
-    .style("stroke", function(d, i) { return 'black'; })
     .call(force.drag);
 
   d3.select("body")
@@ -99,12 +205,30 @@ function showCitations(data){
   }
 
   function tick(e) {
-    var k = 10*e.alpha;
+    var k = width/40*e.alpha;
+    var kh = height/80*e.alpha;
     data.forEach(function(o,i) {
-      if (sortByAuthor == true && o.Author == true){
-        o.x += 2*k;
-      } else if (sortByYear == true){
-        o.x += (2013-parseInt(o.Year))*-1*k;
+      if (sortByAuthorX == true){
+        if (o.Author == true){
+          o.x += -k;
+        } else {
+          o.x += k;
+        }
+      } else if (sortByYearX == true){
+        o.x += (parseInt(o.Year)-(minYear+maxYear)/2)*k*2/(maxYear-minYear);
+      } else if (sortByCitationsX == true){
+        o.x += (o.CitedBy - maxCitations/2)*k*2/maxCitations;
+      }
+      if (sortByAuthorY == true){
+        if (o.Author == true){
+          o.y += kh;
+        } else {
+          o.y += -kh;
+        }
+      } else if (sortByYearY == true){
+        o.y -= (parseInt(o.Year)-(minYear+maxYear)/2)*kh*2/(maxYear-minYear);
+      } else if (sortByCitationsY == true){
+        o.y -= (o.CitedBy - maxCitations/2)*kh*2/maxCitations;
       }
     });
 
